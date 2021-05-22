1edcf577998a79bad0d533316890699ad0959a33
Lee WonJoong <realjoong@gmail.com>
2021-05-23 06:00:06 +0900

    Main Acitivty 완료

--- app/src/main/java/com/example/jinsool/MainActivity/MainActivity.kt
+++ app/src/main/java/com/example/jinsool/MainActivity/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.jinsool.MainActivity
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
 import androidx.core.widget.addTextChangedListener
 import com.example.jinsool.GameActivity.GameActivity
 import com.example.jinsool.R
@@ -27,13 +30,23 @@ class MainActivity : AppCompatActivity() {
             startGameActivity()
         }
 
-        binding.etPlayerCnt.addTextChangedListener(object : TextWatcher{
+        binding.etPlayerCnt.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 //
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 Log.e("@@@", Integer.parseInt(s.toString()).toString())
+                var circleCnt = Integer.parseInt(s.toString())
+
+                for (i in (circleCnt + 1)..8) {
+                    var temp = "iv_circle".plus(i)
+                    val resId = resources.getIdentifier(temp, "id", packageName)
+                    val iv = findViewById<ImageView>(resId)
+                    iv.visibility = View.INVISIBLE
+                    //iv.setBackgroundColor(Color.BLUE)
+
+                }
             }
 
             override fun afterTextChanged(s: Editable?) {
