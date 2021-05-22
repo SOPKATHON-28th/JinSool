125c0b2bb8ab32bddde1b994f0f0bf0bb9425523
onpyeong <gsh4471@gmail.com>
2021-05-23 01:33:37 +0900

    timer 기능 추가

--- app/src/main/java/com/example/jinsool/Question/QuestionFragment.kt
+++ app/src/main/java/com/example/jinsool/Question/QuestionFragment.kt
@@ -1,6 +1,7 @@
 package com.example.jinsool.Question
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +11,7 @@ import com.example.jinsool.GameActivity.GameActivity
 import com.example.jinsool.R
 import com.example.jinsool.databinding.FragmentPenaltyBinding
 import com.example.jinsool.databinding.FragmentQuestionBinding
+import kotlin.math.roundToInt
 
 class QuestionFragment : Fragment() {
     private var _binding: FragmentQuestionBinding? = null
@@ -21,9 +23,26 @@ class QuestionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_question, container, false)
-        binding.btTemp.setOnClickListener {
-            (activity as GameActivity).navigateSelect()
-        }
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        startCountDownTimer()
+    }
+
+    private fun startCountDownTimer() {
+        val countDownTimer : CountDownTimer = object: CountDownTimer(20000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                binding.tvTimer.text = "${(millisUntilFinished.toFloat() / 1000.0f).roundToInt()}"
+            }
+
+            override fun onFinish() {
+                (activity as GameActivity).navigateSelect()
+            }
+
+        }
+        countDownTimer.start()
+    }
+
 }
