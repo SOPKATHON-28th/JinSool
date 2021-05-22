935c2a01585aef710d5cc80ca7680f34a8498445
Lee WonJoong <realjoong@gmail.com>
2021-05-23 05:02:18 +0900

    Main Activity Edit Text 감지부분까지 완료

--- app/src/main/java/com/example/jinsool/MainActivity/MainActivity.kt
+++ app/src/main/java/com/example/jinsool/MainActivity/MainActivity.kt
@@ -1,16 +1,51 @@
 package com.example.jinsool.MainActivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
+import androidx.core.widget.addTextChangedListener
+import com.example.jinsool.GameActivity.GameActivity
 import com.example.jinsool.R
 import com.example.jinsool.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
+
+        binding.ivGreenCircle.setOnClickListener {
+            startGameActivity()
+        }
+
+        binding.ivRedCircle.setOnClickListener {
+            startGameActivity()
+        }
+
+        binding.etPlayerCnt.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                Log.e("@@@", Integer.parseInt(s.toString()).toString())
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                //
+            }
+        })
+
+    }
+
+    private fun startGameActivity() {
+        val intent = Intent(this, GameActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
